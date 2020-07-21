1996d8215398b50719faf80162c59c3f7b7daa7e
maelves <36156196+maelves@users.noreply.github.com>
2020-07-21 15:07:33 +0300

    Add files via upload

---
+++ Lab01/prb04.c
@@ -0,0 +1,14 @@
+// Degrees in Radians
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float d, r;
+    printf("Degrees: ");
+    scanf("%f", &d);
+    r = (d * 3.1415) / 180;
+    printf("Radians: %f\n", r);
+    return 0;
+}
