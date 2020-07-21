a597279ba29a267f3b75c139cd68ed70654cb4de
maelves <36156196+maelves@users.noreply.github.com>
2020-07-21 15:20:20 +0300

    Add files via upload

---
+++ Lab01/prb06.c
@@ -0,0 +1,13 @@
+// Printing the ASCII code of a character given by the user
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    printf("Character: ");
+    scanf("%c", &c);
+    printf("ASCII code: %d\n", c);
+    return 0;
+}
