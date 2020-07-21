a387e1773e95e5f3fed8991520c2a2e756950391
maelves <36156196+maelves@users.noreply.github.com>
2020-07-21 15:22:54 +0300

    Add files via upload

---
+++ Lab01/prb07.c
@@ -0,0 +1,13 @@
+// Printing the character coresponding to a given ASCII code
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c;
+    printf("ASCII code: ");
+    scanf("%d", &c);
+    printf("Character: %c\n", c);
+    return 0;
+}
